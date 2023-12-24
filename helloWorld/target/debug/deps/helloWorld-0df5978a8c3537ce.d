a0d23d01d90f5de3b0a1d472804260f0c341f716
muniss950 <munish3971@gmail.com>
2023-12-24 12:18:29 +0530

    first program

---
+++ helloWorld/target/debug/deps/helloWorld-0df5978a8c3537ce.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/munis/rustProgramming/helloWorld/target/debug/deps/helloWorld-0df5978a8c3537ce: src/main.rs
+
+/mnt/c/Users/munis/rustProgramming/helloWorld/target/debug/deps/helloWorld-0df5978a8c3537ce.d: src/main.rs
+
+src/main.rs:
---
+++ helloWorld/target/debug/helloWorld.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/munis/rustProgramming/helloWorld/target/debug/helloWorld: /mnt/c/Users/munis/rustProgramming/helloWorld/src/main.rs
